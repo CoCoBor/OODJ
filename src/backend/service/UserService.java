b5bf261a12762bc8642d0ffc2f2810702e57606d
Lee <leezhwork@gmail.com>
2026-05-22 00:45:33 +0800

    update validate email

--- src/backend/service/UserService.java
+++ src/backend/service/UserService.java
@@ -321,7 +321,7 @@ public class UserService {
     }
 
     private String validateEmail(String email) {
-        Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[cC][oO][mM]$");
+        Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[c][o][m]$");
         if (!EMAIL_PATTERN.matcher(email).matches()) {
             throw new ServiceException("Invalid email format");
         }
